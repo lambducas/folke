352d10cbb21bf89a93ab4349e1108e7773e6a366
TheCoder5550 <alfons.nilsson@outlook.com>
2025-03-07 16:25:37 +0100

    Added frontend and removed code to compile

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 cabal build
 cabal run monomergui
 ```
+
+### Fix freetype2 not found
+On Ubuntu, to fix `rejecting: nanovg:-stb_truetype (conflict: pkg-config package
+freetype2-any, not found in the pkg-config database)` when building, run
+```bash
+sudo apt-get install libfreetype6-dev
+```
--- app/Example.hs
+++ app/Example.hs
@@ -2,39 +2,43 @@ module Example (
     example
 ) where
 
-import Backend.TypeChecker
-import Parser.Logic.Abs
-import qualified Data.Map as Map
-
 example :: IO ()
 example = do
-    let env = Map.empty
-        termX = Term (TermId "x") (Params [])
-        termY = Term (TermId "y") (Params [])
-        termA = Term (TermId "a") (Params [])
-        termB = Term (TermId "b") (Params [])
+    print "Build error :)"
+
+-- import Backend.TypeChecker
+-- import Parser.Logic.Abs
+-- import qualified Data.Map as Map
+
+-- example :: IO ()
+-- example = do
+--     let env = Map.empty
+--         termX = Term (TermId "x") (Params [])
+--         termY = Term (TermId "y") (Params [])
+--         termA = Term (TermId "a") (Params [])
+--         termB = Term (TermId "b") (Params [])
         
-        -- Statements
-        form1 = FormEq termX termY
-        form2 = FormNot (FormEq termA termB)
+--         -- Statements
+--         form1 = FormEq termX termY
+--         form2 = FormNot (FormEq termA termB)
         
-        -- Conclusion
-        conclusion = FormAnd form1 form2
+--         -- Conclusion
+--         conclusion = FormAnd form1 form2
         
-        -- Steps
-        steps = [ StepPrem form1
-                , StepAssume form2
-                , StepForm (RuleId "someRule") [ArgLit 1] conclusion
-                ]
+--         -- Steps
+--         steps = [ StepPrem form1
+--                 , StepAssume form2
+--                 , StepForm (RuleId "someRule") [ArgLit 1] conclusion
+--                 ]
         
-        -- Sequent
-        sequent = Seq [form1, form2] conclusion steps
+--         -- Sequent
+--         sequent = Seq [form1, form2] conclusion steps
         
-        -- Check the sequent
-        result = checkSequent env sequent
+--         -- Check the sequent
+--         result = checkSequent env sequent
     
-    case result of
-        Left err -> putStrLn $ "Error: " ++ err
-        Right (checkedSequent, newEnv) -> do
-            putStrLn $ "Checked Sequent: " ++ show checkedSequent
-            putStrLn $ "New Environment: " ++ show newEnv
+--     case result of
+--         Left err -> putStrLn $ "Error: " ++ err
+--         Right (checkedSequent, newEnv) -> do
+--             putStrLn $ "Checked Sequent: " ++ show checkedSequent
+--             putStrLn $ "New Environment: " ++ show newEnv
