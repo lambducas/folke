601c2f04cf068b6093c28e417fbd1f03a42f75c0
Anton Nordbeck <anton.nordbeck@gmail.com>
2025-03-21 20:17:32 +0100

    removed name confict

--- app/Example.hs
+++ app/Example.hs
@@ -3,33 +3,33 @@ module Example (
 ) where
 
 import Backend.TypeChecker
-import Logic.Abs
+import qualified Logic.Abs as Abs
 import Backend.Types
 
 example :: IO ()
 example = do
-    let termX = Term (Ident "x") (Params [])
-        termY = Term (Ident "y") (Params [])
-        termZ = Term (Ident "z") (Params [])
+    let termX = Abs.Term (Abs.Ident "x") (Abs.Params [])
+        termY = Abs.Term (Abs.Ident "y") (Abs.Params [])
+        termZ = Abs.Term (Abs.Ident "z") (Abs.Params [])
         
         -- Statements
-        form1 = FormEq termX termY
-        form2 = FormEq termY termZ
-        form3 = FormEq termX termZ
+        form1 = Abs.FormEq termX termY
+        form2 = Abs.FormEq termY termZ
+        form3 = Abs.FormEq termX termZ
         
         -- Conclusion
         conclusion = form3
         
         -- Steps
-        steps = [ StepPrem form1
-                , StepPrem form2
-                , StepForm (Ident "equalityElim") [ArgLit 1, ArgLit 2] conclusion
+        steps = [ Abs.StepPrem form1
+                , Abs.StepPrem form2
+                , Abs.StepForm (Abs.Ident "equalityElim") [Abs.ArgLit 1, Abs.ArgLit 2] conclusion
                 ]
 
-        proof = Proof (map (ProofElem []) steps)
+        proof = Abs.Proof (map (Abs.ProofElem []) steps)
         
         -- Sequent
-        sequent = Seq [form1, form2] conclusion proof
+        sequent = Abs.Seq [form1, form2] conclusion proof
         
         -- Check the sequent
         result = check sequent
