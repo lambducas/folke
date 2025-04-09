0b9bc2dbd26b2c92e1668cedd8b39e281e539f2a
TheCoder5550 <alfons.nilsson@outlook.com>
2025-04-09 20:09:21 +0200

    Example is correct now

--- app/Example.hs
+++ app/Example.hs
@@ -2,8 +2,8 @@ module Example (
     example
 ) where
 
-import Backend.TypeChecker
-import Backend.Types
+import Backend.TypeChecker (check)
+import Backend.Types (Result(Ok, Error))
 import qualified Logic.Abs as Abs
 
 example :: IO ()
@@ -11,19 +11,19 @@ example = do
     let termX = Abs.Term (Abs.Ident "x") (Abs.Params [])
         termY = Abs.Term (Abs.Ident "y") (Abs.Params [])
         termZ = Abs.Term (Abs.Ident "z") (Abs.Params [])
+        termU = Abs.Term (Abs.Ident "u") (Abs.Params [])
         
         -- Statements
         form1 = Abs.FormEq termX termY
         form2 = Abs.FormEq termY termZ
-        form3 = Abs.FormEq termX termZ
         
         -- Conclusion
-        conclusion = form3
+        conclusion = Abs.FormEq termX termZ
         
         -- Steps
         steps = [ Abs.StepPrem form1
                 , Abs.StepPrem form2
-                , Abs.StepForm (Abs.Ident "EqE") [Abs.ArgLine 1, Abs.ArgLine 2] conclusion
+                , Abs.StepForm (Abs.Ident "EqE") [Abs.ArgLine 2, Abs.ArgLine 1, Abs.ArgForm termU (Abs.FormEq termX termU)] conclusion
                 ]
 
         proof = Abs.Proof (zipWith (\s i -> Abs.ProofElem [Abs.LabelLine i] s) steps [1..])
